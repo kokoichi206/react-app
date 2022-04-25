a4ab5ddf28aedba1124bf1f37c81a468e165e02a
takahiro.tominaga <tt.300607@gmail.com>
2022-04-25 22:51:32 +0900

    done

--- mycalc/src/logic/calclate.ts
+++ mycalc/src/logic/calclate.ts
@@ -1,4 +1,32 @@
+import { stat } from "fs"
+
 export function calculate(button: string, state: State): State {
+  if (state.isNextClear) {
+    return {
+      current: button,
+      operand: state.operand,
+      operator: state.operator,
+      isNextClear: false,
+    }
+  }
+  if (isNumberButton(button)) {
+    return handleNumberButton(button, state)
+  }
+  if (isOperatorButton(button)) {
+    return handleOperatorButton(button, state)
+  }
+  if (isDotButton(button)) {
+    return handleDotButton(state)
+  }
+  if (isDeleteButton(button)) {
+    return handleDeleteButton(state)
+  }
+  if (isAllClearButton(button)) {
+    return handleAllClearButton()
+  }
+  if (isEqualButton(button)) {
+    return handleEqualButton(state)
+  }
   return state;
 }
 
@@ -8,3 +36,148 @@ export interface State {
   operator: string | null;
   isNextClear: boolean;
 }
+
+function isNumberButton(button: string) {
+  return (
+    button === "0" || 
+    button === "1" ||
+    button === "2" ||
+    button === "8" ||
+    button === "9"
+  );
+}
+
+/**
+ * 数字キーが押された時の状態変化
+ * 
+ * @param button 
+ * @param state 
+ * @returns 
+ */
+function handleNumberButton(button: string, state: State): State {
+  if (state.current === "0") {
+    return {
+      current: button,
+      operand: state.operand,
+      operator: state.operator,
+      isNextClear: false,
+    }
+  }
+  return {
+    current: state.current + button,
+    operand: state.operand,
+    operator: state.operator,
+    isNextClear: false,
+  }
+}
+
+function isOperatorButton(button: string) {
+  return (
+    button === "+" ||
+    button === "-"
+  )
+}
+
+function handleOperatorButton(button: string, state: State): State {
+  if (state.operator === null) {
+    return {
+      current: state.current,
+      operand: parseFloat(state.current),
+      operator: button,
+      isNextClear: true,
+    }
+  }
+  const nextValue = operate(state)
+  return {
+    current: `${nextValue}`,
+    operand: parseFloat(state.current),
+    operator: button,
+    isNextClear: true,
+  }
+}
+
+
+function isDotButton(button: string) {
+  return button === ".";
+}
+
+function handleDotButton(state: State): State {
+  // すでにドットが存在する場合
+  if (state.current.indexOf('.') !== -1) {
+    return state;
+  }
+  return {
+    current: state.current + ".",
+    operand: state.operand,
+    operator: state.operator,
+    isNextClear: false,
+  }
+}
+
+
+function isDeleteButton(button: string) {
+  return button === "D";
+}
+
+function handleDeleteButton(state: State): State {
+  if (state.current.length === 1) {
+    return {
+      current: "0",
+      operand: state.operand,
+      operator: state.operator,
+      isNextClear: false,
+    }
+  }
+  return {
+    current: state.current.substring(0, state.current.length - 1),
+    operand: state.operand,
+    operator: state.operator,
+    isNextClear: false,
+  }
+}
+
+
+function isAllClearButton(button: string) {
+  return button === "AC";
+}
+
+function handleAllClearButton(): State {
+  return {
+    current: "0",
+    operand: 0,
+    operator: null,
+    isNextClear: false,
+  }
+}
+
+
+function isEqualButton(button: string) {
+  return button === "=";
+}
+
+function handleEqualButton(state: State): State {
+  if (state.operator === null) {
+    return state
+  }
+  const nextValue = operate(state)
+  return {
+    current: `${nextValue}`,
+    operand: 0,
+    operator: null,
+    isNextClear: false,
+  }
+}
+
+
+
+function operate(state: State): number {
+  const current = parseFloat(state.current);
+  if (state.operator === "+") {
+    return state.operand + current;
+  }
+  if (state.operator === "-") {
+    return state.operand - current;
+  }
+  return current;
+}
+
