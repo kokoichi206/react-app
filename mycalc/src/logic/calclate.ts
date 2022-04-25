f19c318e306d92279857bd19d96e9a446ff5ecd8
takahiro.tominaga <tt.300607@gmail.com>
2022-04-25 22:29:08 +0900

    e

---
+++ mycalc/src/logic/calclate.ts
@@ -0,0 +1,10 @@
+export function calculate(button: string, state: State): State {
+  return state;
+}
+
+export interface State {
+  current: string;
+  operand: number;
+  operator: string | null;
+  isNextClear: boolean;
+}
