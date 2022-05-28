1b09b76d638e0d7b2ce7c26831340ab1dfb94b4a
takahiro.tominaga <tt.300607@gmail.com>
2022-05-28 15:53:46 +0900

    feat: draggable in draggable

--- drag-and-drop/README.md
+++ drag-and-drop/README.md
@@ -1,6 +1,7 @@
 # ドラッグ&ドロップ
 
-[参考にした youtube](https://www.youtube.com/watch?v=1amkEbOZM8M&ab_channel=%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%81%E3%83%A5%E3%83%BC%E3%83%88%E3%83%AA%E3%82%A2%E3%83%AB)
+https://user-images.githubusercontent.com/52474650/170814160-8e9fbf96-fb31-4434-8547-c8e1279ea508.mov
+
 
 ## [react-beautiful-dnd](https://github.com/atlassian/react-beautiful-dnd)
 ``` sh
@@ -24,4 +25,17 @@ root.render(
   // </React.StrictMode>
 );
 ```
+
+
+## [Board](https://codesandbox.io/s/ym3rvj1z71?file=/src/index.js:1107-1112)
+自分でボードっぽいの作れたけど、ここでは Column から Column への移動も実現できている。。。
+
+本格的に何か作る時はここを参考にしつつ、列間での移動を実現させたい。
+
+
+## Links
+- [react-beautiful-dnd](https://github.com/atlassian/react-beautiful-dnd)
+- [参考にした youtube](https://www.youtube.com/watch?v=1amkEbOZM8M&ab_channel=%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%81%E3%83%A5%E3%83%BC%E3%83%88%E3%83%AA%E3%82%A2%E3%83%AB)
+- [horizontal example](https://codesandbox.io/s/mmrp44okvj?file=/index.js:1814-1836)
+- [Board](https://codesandbox.io/s/ym3rvj1z71?file=/src/index.js:1107-1112)
 
--- drag-and-drop/src/App.js
+++ drag-and-drop/src/App.js
@@ -1,7 +1,7 @@
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import "./App.css";
 
-const items = [
+const items1 = [
   {
     id: "item-0",
     content: "item 0",
@@ -24,7 +24,39 @@ const items = [
   },
 ];
 
+const items2 = [
+  {
+    id: "item2-0",
+    content: "item2 0",
+  },
+  {
+    id: "item2-1",
+    content: "item2 1",
+  },
+  {
+    id: "item2-2",
+    content: "item2 2",
+  },
+];
+
+const itemsGroup = [
+  {
+    id: "group1",
+    items: items1,
+  },
+  {
+    id: "group2",
+    items: items2,
+  },
+];
+
+let draggingGroup = items1;
+
 const grid = 8;
+
+const getListGroupStyle = () => ({
+  display: "flex",
+});
 
 const getListStyle = (isDraggingOver) => ({
   background: isDraggingOver ? "lightblue" : "lightgrey",
@@ -48,26 +80,48 @@ const reorder = (list, startIndex, endIndex) => {
 };
 
 function App() {
-  const onDragEnd = (result) => {
+  const onDragEndGroup = (result) => {
     // console.log(result);
     if (!result.destination) {
       return;
     }
-    reorder(items, result.source.index, result.destination.index);
+    reorder(itemsGroup, result.source.index, result.destination.index);
+  };
+
+  const getItemList = (id) => {
+    // console.log("id: " + id);
+    for (const items of itemsGroup) {
+      // console.log(items);
+      if (id == items.id) {
+        return items.items;
+      }
+    }
+    return null;
+  };
+
+  const onDragEndItems = (result) => {
+    console.log(result);
+    if (!result.destination) {
+      return;
+    }
+    const items = getItemList(result.source.droppableId);
+    if (items) {
+      reorder(items, result.source.index, result.destination.index);
+    }
   };
 
   return (
     <div>
-      <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="droppable">
+      <DragDropContext onDragEnd={onDragEndGroup}>
+        <Droppable droppableId="droppable" direction="horizontal">
           {(provided, snapshot) => (
             <div
               {...provided.droppableProps}
               ref={provided.innerRef}
-              style={getListStyle(snapshot.isDraggingOver)}
+              style={getListGroupStyle()}
             >
-              {items.map((item, index) => (
-                <Draggable key={item.id} draggableId={item.id} index={index}>
+              {itemsGroup.map((items, index) => (
+                <Draggable key={items.id} draggableId={items.id} index={index}>
                   {/* snapshot はスタイルを当てるためのもの */}
                   {(provided, snapshot) => (
                     <div
@@ -79,7 +133,44 @@ function App() {
                         provided.draggableProps.style
                       )}
                     >
-                      {item.content}
+                      {/* {item.content} */}
+
+                      <DragDropContext onDragEnd={onDragEndItems}>
+                        <Droppable droppableId={items.id}>
+                          {(provided, snapshot) => (
+                            <div
+                              {...provided.droppableProps}
+                              ref={provided.innerRef}
+                              style={getListStyle(snapshot.isDraggingOver)}
+                            >
+                              {items.items.map((item, index) => (
+                                <Draggable
+                                  key={item.id}
+                                  draggableId={item.id}
+                                  index={index}
+                                >
+                                  {/* snapshot はスタイルを当てるためのもの */}
+                                  {(provided, snapshot) => (
+                                    <div
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={getItemStyle(
+                                        snapshot.isDragging,
+                                        provided.draggableProps.style
+                                      )}
+                                    >
+                                      {item.content}
+                                    </div>
+                                  )}
+                                </Draggable>
+                              ))}
+                              {/* 移動中のところもスタイルを適応する */}
+                              {provided.placeholder}
+                            </div>
+                          )}
+                        </Droppable>
+                      </DragDropContext>
                     </div>
                   )}
                 </Draggable>
