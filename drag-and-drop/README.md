baf3efe94d7132eff3064643c432a053092c06fd
takahiro.tominaga <tt.300607@gmail.com>
2022-05-28 14:18:33 +0900

    feat: make div draggable

--- drag-and-drop/README.md
+++ drag-and-drop/README.md
@@ -1,3 +1,27 @@
 # ドラッグ&ドロップ
 
 [参考にした youtube](https://www.youtube.com/watch?v=1amkEbOZM8M&ab_channel=%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%81%E3%83%A5%E3%83%BC%E3%83%88%E3%83%AA%E3%82%A2%E3%83%AB)
+
+## [react-beautiful-dnd](https://github.com/atlassian/react-beautiful-dnd)
+``` sh
+npm install react-beautiful-dnd
+```
+
+[API](https://github.com/atlassian/react-beautiful-dnd#api-%EF%B8%8F)の部分にあるが、３つのコンポーネントが必要
+
+
+## Unable to find draggable with id: item-3👷‍
+```
+Unable to find draggable with id: item-3👷‍ This is a development only message. It will be removed in production builds.
+```
+
+のようなエラーが出ていて、ドラッグできなかった
+
+``` react
+root.render(
+  // <React.StrictMode>
+    <App />
+  // </React.StrictMode>
+);
+```
+
--- drag-and-drop/src/App.js
+++ drag-and-drop/src/App.js
@@ -1,8 +1,56 @@
-import logo from "./logo.svg";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import "./App.css";
 
+const items = [
+  {
+    id: "item-0",
+    content: "item 0",
+  },
+  {
+    id: "item-1",
+    content: "item 1",
+  },
+  {
+    id: "item-2",
+    content: "item 2",
+  },
+  {
+    id: "item-3",
+    content: "item 3",
+  },
+  {
+    id: "item-4",
+    content: "item 4",
+  },
+];
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <DragDropContext>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {items.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {/* snapshot はスタイルを当てるためのもの */}
+                  {(provided, snapshot) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 }
 
 export default App;
